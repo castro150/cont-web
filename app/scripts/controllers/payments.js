23dd775cdde08e5cfab30da100bb9496c4561af1
Rafael Castro <rafaelcarneiroget@hotmail.com>
2016-07-11 18:19:27 -0300

    Combo funcional

--- app/scripts/controllers/payments.js
+++ app/scripts/controllers/payments.js
@@ -12,6 +12,39 @@
 	function PaymentsCtrl() {
 
 		var ctrl = this;
-		
+
+		ctrl.querySearch = querySearch;
+
+		// ******************************
+		// Init method
+		// ******************************
+		init();
+		function init() {
+			ctrl.companies = loadCompanies();
+		}
+
+		// ******************************
+		// Public methods
+		// ******************************
+		function querySearch(text) {
+			return text ? ctrl.companies.filter(function(company) {
+				return company.value.indexOf(angular.lowercase(text)) === 0;
+			}) : ctrl.companies;
+		}
+
+		// ******************************
+		// Private methods
+		// ******************************
+		function loadCompanies() {
+			// TODO fazer o serviço para retornar as empresas.
+			var companies = ['Empresa1', 'Empresa2', 'Empresa3'];
+			return companies.map(function(company) {
+				return {
+					value: company.toLowerCase(),
+					display: company
+				};
+			});
+		}
+
 	}
 })();
