78505406b7355739ccee434457330f7ee70e9fa7
castro150 <rafaelcarneiroget@hotmail.com>
2017-04-04 08:49:03 -0300

    Removendo .run()

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,13 +20,5 @@
 					    },
 					    'preferredLocale': 'pt_BR'
 						}
-					)
-					.run(['$rootScope', '$log', function($rootScope, $log) {
-						$rootScope.$on('$stateChangeStart', function(event, next) {
-							// TODO se não estiver logado e não for estado de login:
-							// TODO chamar o serviço de segurança, que vai acionar o evento.
-							$log.info('Changeing state. ' + event + next);
-						});
-						// TODO tratar os eventos acionados pelo serviço de segurança.
-					}]);
+					);
 })();
